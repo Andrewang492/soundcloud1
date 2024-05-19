ea5d5d2b9ff89efb95cfa89de92f3f1f30b00e4b
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2024-05-19 17:15:33 +1000

    updated configuartion to allow me to fetch soundcloud keys.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 import EmbeddedPlayer from "./components/EmbeddedPlayer";
 import Widget from "./components/Widget";
+import { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+
 function App() {
   //# sourceMappingURL=http://ent/web-sourcemaps/api.js-27d0ec1de3c5.map
   // var iframeElement   = document.querySelector('iframe');
   // var iframeElementID = iframeElement.id;
   // var widget1         = SC.Widget(iframeElement);
   // var widget2         = SC.Widget(iframeElementID);
+  const [query, setQuery] = useState("enter song name"); //what is visible on screen
+  const [queryDelayed, setDelayedQuery] = useState(""); // what query is used on my functions. Does not update as frequently.
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      console.log(`query is ${query}`);
+      setDelayedQuery(query);
+    }, 500);
+
+    // Cleanup function that cancels the timeout if the effect is re-run before it completes
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [query]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -24,8 +39,15 @@ function App() {
         >
           Learn React
         </a>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
         <Widget />
-        <EmbeddedPlayer />
+        <EmbeddedPlayer query={queryDelayed} />
       </header>
     </div>
   );
--- src/scripts/keyfetch.js
+++ src/scripts/keyfetch.js
@@ -2,17 +2,43 @@
 
 const sckey = require("soundcloud-key-fetch");
 
-const key = sckey.fetchKey().then((key) => {
-  console.log(key);
-  return key;
-});
-
-key
-  .then((key) => sckey.testKey(key))
-  .then((result) => {
-    if (result) {
-      console.log("key works");
-    } else {
-      console.log("key doesnt work");
-    }
+function test() {
+  const key = sckey.fetchKey().then((key) => {
+    console.log(key);
+    return key;
   });
+
+  key
+    .then((key) => sckey.testKey(key))
+    .then((result) => {
+      if (result) {
+        console.log("key works");
+      } else {
+        console.log("key doesnt work");
+      }
+    });
+
+  key
+    .then((clientid) => {
+      return fetch(
+        `https://api-v2.soundcloud.com/search?client_id=${clientid}&q=the%20buzz&limit=1`
+      );
+    })
+    .then((jsonres) => jsonres.json())
+    .then((obj) => console.log(obj));
+}
+
+/**
+ * Returns a promise (giving a json body).
+ */
+
+export function search(query) {
+  sckey
+    .fetchKey()
+    .then((clientid) => {
+      return fetch(
+        `https://api-v2.soundcloud.com/search?client_id=${clientid}&q=${query}&limit=1`
+      );
+    })
+    .then((jsonres) => jsonres.json());
+}
